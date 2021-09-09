975fd71a3145b0a6bb30fd40189def8f5c4c3295
REBECA LOPES DE OLIVEIRA <21276568@CFP127ALUNO.LOCAL>
2021-09-09 15:11:39 -0300

    Projetinho concluido

--- src/area/AreaCasa.java
+++ src/area/AreaCasa.java
@@ -7,26 +7,68 @@ public class AreaCasa {
 		
 Scanner leitor = new Scanner (System.in);
 		
-		String nome, resposta ;
+		String nome;
+		String resposta = "S";
 		double largura, comprimento;
 		int numeroRepeticao = 0, contador = 0;
+		
 	
+		System.out.println("**************************");
+		System.out.println(" Calculo área de uma casa ");
+		System.out.println("**************************");
+		
+		System.out.println();
 		
 		System.out.print("De qual cômodo você deseja saber a area? ");
 		nome = leitor.next();
 		
-		System.out.print("Qual é a largura em metros do cômodo? ");
+		System.out.print("Qual é a largura em metros do(a) " + nome + "? ");
 		largura = leitor.nextInt();
-		System.out.print("Qual é o comprimento em metros do cômodo? ");
-		comprimento = leitor.nextInt();
-		System.out.println("A " +  nome + " tem " + (largura * comprimento) + " metros quadrados");
 		
-		System.out.println("Você deseja calcular mais cômodos?");
+		System.out.print("Qual é o comprimento em metros do(a) " + nome + "? " );
+		comprimento = leitor.nextInt();
+		
+		System.out.println();
+		
+		System.out.println("O(A) " +  nome + " TEM " + (largura * comprimento) + " METROS QUADRADOS " );
+		
+		System.out.println();
+		
+		System.out.println("Você deseja calcular mais cômodos? (S/N)");
 		resposta = leitor.next();
 		
-		while () {
+		 while (resposta == "S");
+		 if (resposta.equals("S")) {
 			
+			System.out.println();
+			System.out.print("De qual cômodo você deseja saber a area? ");
+			nome = leitor.next();
+			
+			System.out.print("Qual é a largura em metros do(a) " + nome + "? ");
+			largura = leitor.nextInt();
+			
+			System.out.print("Qual é o comprimento em metros do cômodo?  " );
+			comprimento = leitor.nextInt();
+			
+			System.out.println();
+			
+			System.out.println("O(A) " +  nome + " TEM " + (largura * comprimento) + " METROS QUADRADOS " );
+			
+			System.out.println();
+			
+			System.out.println("Você deseja calcular mais cômodos? (S/N)");
+			resposta = leitor.next();
+		
+			
+		} else {
+		  System.out.println("A área total da casa é 38 metros quadrados.");
+		  System.out.println("*********************FIM*******************");
 		}
+		
+		
+		
+		
+	
 	
 		
 		
